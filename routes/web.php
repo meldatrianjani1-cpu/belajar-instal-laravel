9184da484f9b83e290f9245a022962a95cb1a704
Syaira Safa Queena <queensyaira09@gmail.com>
2026-08-31 13:49:33 +0700

    fix

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RouteController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [RouteController::class, 'index']);
+
+Route::get('/form', [RouteController::class, 'form']);
+
+Route::get('/welcome', [RouteController::class, 'welcome']);
+
+Route::get('/data', [RouteController::class, 'data']);
+
+
+
